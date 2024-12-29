e203312f37d11e7da8f48f805cb75ce2058bdcf5
spencerhum04 <spencerhum04@gmail.com>
2024-12-29 10:36:52 -0500

    fix: responsiveness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
     return (
-        <div className="w-full text-white text-center text-sm bg-black py-4">Copyright 2024 Spencer Hum. All rights reserved.</div>
+        <div className="w-full text-black text-center text-sm py-4">Copyright 2024 Spencer Hum. All rights reserved.</div>
     )
 }
