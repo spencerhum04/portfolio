184e2d9e11294e4c460e1c91f06c410c3dc480dd
Spencer Hum <spencerhum@Spencers-MacBook-Air.local>
2025-01-07 19:51:25 -0500

    feat: layout

--- src/routes/landing/sections/experience.tsx
+++ src/routes/landing/sections/experience.tsx
@@ -1,26 +1,24 @@
+import '../landing.css';
 import canadaComputers from "../../../assets/canada-computers.png";
 
 const companies = [
-    { image: canadaComputers, name: "Canada Computers & Electronics", role: "Business Applications Developer", type: "Internship", date: "Sept. 2024 - Dec. 2024", location: "Richmond Hill, ON"  },
+    { image: canadaComputers, name: "Canada Computers & Electronics", role: "Business Applications Developer · Internship", date: "Sept. 2024 - Dec. 2024"},
 ]
 
 export default function Experience() {
     return (
-        <div className="flex flex-col items-center gap-y-10 py-20 h-[1000px] overflow-hidden">
-            <div className="text-3xl font-medium text-center">Experience</div>
-                <div className="flex flex-col">
-                {companies.map((company) => (
-                    <div className="flex flex-row gap-x-10">
-                        <img src={canadaComputers} className="h-60" />
-                        <div className="flex flex-col">
-                            <div>{company.name}</div>
-                            <div>{company.role}</div>
-                            <div>{company.type}</div>
+        <div className="flex flex-col items-center px-6 py-20 h-[1000px]">
+            <div className="header font-bold text-center mb-4">My Professional Experience.</div>
+            <div className="subheader text-darkgray text-center">As a student, I'm eager to expand my knowledge and gain diverse experience.</div>
+            <div className="w-full flex flex-wrap place-content-center gap-10 mt-12">
+                {companies.map((company, index) => (
+                    <div key={index} className="flex flex-col bg-white px-5 pt-5 rounded-xl shadow-xl w-full max-w-[614px] text-center">
+                        <div className="flex flex-col mt-4">
+                            <div className="text-3xl font-bold">{company.name}</div>
+                            <div className="text-darkgray p-4">{company.role}</div>
+                            <div className="text-lg text-emerald-600 mb-10">{company.date}</div>
                         </div>
-                        <div className="flex flex-col">
-                            <div>{company.date}</div>
-                            <div>{company.location}</div>
-                        </div>
+                        <img src={company.image} className="w-full h-80 object-cover" />
                     </div>
                 ))}
             </div>
--- src/routes/landing/sections/hero.tsx
+++ src/routes/landing/sections/hero.tsx
@@ -5,6 +5,7 @@ import fish from "../../../assets/fish.png";
 
 export default function Hero() {
     const [opacity, setOpacity] = useState(1);
+    const [visible, setVisible] = useState(true);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -12,6 +13,7 @@ export default function Hero() {
             const fadePoint = 400;
             const newOpacity = Math.max(1 - scrollY / fadePoint, 0);
             setOpacity(newOpacity);
+            setVisible(newOpacity > 0);
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -22,14 +24,16 @@ export default function Hero() {
 
     return (
         <div>
-            <div className="fixed w-full h-[600px] bg-black text-white flex flex-col items-center justify-center text-center px-8" style={{ opacity }}>
-                <div className="text-3xl lg:text-5xl font-medium">hey there im spencer</div>
-                <div className="text-lg lg:text-xl text-subgray font-medium mt-4">full stack developer</div>
-                <div className="w-full flex flex-wrap justify-center gap-x-8 gap-y-4 mt-12 font-medium">
-                    <a href="/Spencer Hum - Resume.pdf" target="_blank" rel="noopener noreferrer" className="place-content-center text-white w-full max-w-48 h-10 bg-emerald-600 hover:bg-emerald-700 rounded">Resume</a>
-                    <a href="https://github.com/spencerhum04" target="_blank" rel="noopener noreferrer" className="place-content-center text-emerald-600 w-full max-w-48 h-10 border border-emerald-600 hover:bg-emerald-950 rounded">GitHub</a>
+            {visible &&
+                <div className="fixed w-full h-[600px] bg-black text-white flex flex-col items-center justify-center text-center px-8" style={{ opacity }}>
+                    <div className="text-3xl lg:text-5xl font-medium">hey there im spencer</div>
+                    <div className="text-lg lg:text-xl text-subgray font-medium mt-4">full stack developer</div>
+                    <div className="w-full flex flex-wrap justify-center gap-x-8 gap-y-4 mt-12 font-medium">
+                        <a href="/Spencer Hum - Resume.pdf" target="_blank" rel="noopener noreferrer" className="place-content-center text-white w-full max-w-48 h-10 bg-emerald-600 hover:bg-emerald-700 rounded">Resume</a>
+                        <a href="https://github.com/spencerhum04" target="_blank" rel="noopener noreferrer" className="place-content-center text-emerald-600 w-full max-w-48 h-10 border border-emerald-600 hover:bg-emerald-950 rounded">GitHub</a>
+                    </div>
                 </div>
-            </div>
+                }
             <div className="max-w-[2200px] relative mx-auto place-items-center justify-center pt-[500px] sm:pt-[437.3px] md:pt-[467.3px] lg:pt-[514.3px] z-40" style={{ pointerEvents: opacity > 0 ? "none" : "auto" }}>
                 <div className="w-full h-[250px] sm:h-[600px] flex items-center justify-center px-8">
                     <img src={islands} className="absolute left-0 hidden xl:block h-[435px] object-contain" />
