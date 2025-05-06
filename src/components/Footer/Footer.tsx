8223f1b2b75765f36e4d450de92ee495650b460b
spencerhum04 <spencerhum04@gmail.com>
2025-05-06 17:45:28 -0400

    fix: copyright date

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
     return (
-        <div className="w-full text-black text-center text-sm py-4">Copyright 2024 Spencer Hum. All rights reserved.</div>
+        <div className="w-full text-black text-center text-sm py-4">Copyright 2025 Spencer Hum. All rights reserved.</div>
     )
 }
